f2c81a7714a11fb6e53a0768e0f1056cf23c97bc
Salah Iddine <SalahIddine.Diouri@usmba.ac.ma>
2022-05-20 02:15:40 +0100

    update

--- Modules/__init__.py
+++ Modules/__init__.py
@@ -31,23 +31,27 @@ class Config_Window(QWidget):
         self.project_type_label.setText("Project Type :     ")
         self.project_type_liste = QComboBox(self)
         self.projcet_type_list_data = [
-            "Desktop app Gui",
-            "Desktop app console",
-            "Web app"
+            "Python",
+            "Java",
+            "C",
+            "Cpp",
+            "C#",
+            "Golang",
+            "Ruby",
+            "Rust",
+            "Web app (Html,Css,Js/Ts)"
         ]
         self.project_type_liste.addItems(self.projcet_type_list_data)
         self.project_type_liste.activated.connect(self.sub_liste_implement)
-        #choice add files :
-        self.project_type_meta_data = [
-            ["Tkinter","PyQt5","Kivy","C#","JavaSwing","JavaFx"],
-            ["Cpp","C","Java","Python","Go"],
-            ["Html",'Css','Sass',"Js",'Ts']
-        ]
-        self.add_sup_files_label = QLabel(self)
-        self.add_sup_files_label.setText("Add files to your project :   ")
-        self.add_sup_files_liste = QComboBox(self)
-        #self.add_sup_files_liste.addItems(self.project_type_meta_data[0])
-        self.add_sup_files_liste.setEditable(False)
+        #choice config  :
+        self.add_user_config_label = QLabel(self)
+        self.add_user_config_label.setText(" Project config  :   ")
+        self.user_config_default = QPushButton(self)
+        self.user_config_default.setText("Default config ")
+        self.user_config_default.clicked.connect(self.configuration_default)
+        self.user_config_personalised = QPushButton(self)
+        self.user_config_personalised.setText("user config ")
+        self.user_config_personalised.clicked.connect(self.personalized_config)
         #place widget in the main window :
         self.project_name_label.move(10,10)
         self.project_name_entry.move(20,40)
@@ -56,8 +60,9 @@ class Config_Window(QWidget):
         self.project_folder_btn_select.move(300,150)
         self.project_type_label.move(10,180)
         self.project_type_liste.move(20,210)
-        self.add_sup_files_label.move(10,260)
-        self.add_sup_files_liste.move(20,290)
+        self.add_user_config_label.move(10,260)
+        self.user_config_default.move(20,290)
+        self.user_config_personalised.move(150,290)
         # add css style :
         self.setStyleSheet("""
         background-color:"#FAFAFA";
@@ -108,18 +113,22 @@ class Config_Window(QWidget):
         border-radius: 5px;
         color:"#004268";
         """)
-        self.add_sup_files_label.setStyleSheet("""
+        self.add_user_config_label.setStyleSheet("""
         background-color:"#FAFAFA";
         color:"#004268";
         font-size:15px;
         """)
-        self.add_sup_files_liste.setStyleSheet("""
-        font-size : 10px;
-        width:350px;
-        height:20px;
+        self.user_config_default.setStyleSheet("""
+        color:"#FAFAFA";
+        background-color:"#004268";
+        font-size:15px;
+        border-radius: 5px;
+        """)
+        self.user_config_personalised.setStyleSheet("""
+        font-size : 15px;
+        color:"#004268";
         border:1px solid #004268;
         border-radius: 5px;
-        color:"#004268";
         """)
     def sub_liste_implement(self):
         print("detected :)  ")
@@ -133,7 +142,12 @@ class Config_Window(QWidget):
             self.path = dlg.selectedFiles()[0]
             self.project_folder_entry.setText(self.path)
         return
-
+    def configuration_default(self):
+        print("Default config :)    ")
+        return
+    def personalized_config(self):
+        print("user config :)    ")
+        return
     def start(self):
         self.show()
         return
